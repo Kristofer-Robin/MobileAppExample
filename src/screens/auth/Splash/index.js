44fb63b2601f0c38651aa70c57059a4ce2185533
Student <student@LT-27588.khk.local>
2023-09-27 11:12:17 +0300

    ex2 completed

---
+++ src/screens/auth/Splash/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+    Text,
+    View,
+    Image
+} from "react-native"
+import { styles } from "./styles";
+
+const Splash = () => {
+    return (
+        <View styles={styles.container}>
+            <Image resizeMode="contain" style={styles.image} source={require
+            ('../../../assets/10050.png')}/>
+            <Text style={styles.title}>You'll Find</Text>
+            <Text style={[styles.title, styles.innerTitle]}> All you need </Text>
+            <Text style={styles.title}> Here!</Text>
+        </View>
+    )
+}
+
+export default Splash
