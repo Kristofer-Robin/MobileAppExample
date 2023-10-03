6c666228e5c539674dccd824f4e8db8abdf56194
Student <student@LT-27588.khk.local>
2023-10-03 09:08:01 +0300

    Create color managment file

---
+++ src/components/Button/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { TouchableOpacity, Text, Pressable } from "react-native";
+
+import { styles } from './styles';
+
+const Button = ({title, onPress}) => {
+    const handlePress = () => {
+        console.log('button is clicked')
+    }
+    
+    
+    return(
+        <TouchableOpacity hitSlop={0.6} onPress={onPress} style={styles.container}>
+            <Text style={styles.title}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default Button
---
+++ src/screens/auth/Splash/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+    Text,
+    View,
+    Image,
+    Pressable
+} from "react-native"
+import Button from "../../../components/Button"
+import { styles } from "./styles";
+
+const Splash = () => {
+    return (
+        <View style={styles.container}>
+            <Image resizeMode="contain" style={styles.image} source={require
+            ('../../../assets/10050.png')}/>
+
+            <View style={styles.container}>
+            <Text style={styles.title}>You'll Find</Text>
+            <Text style={[styles.title, styles.innerTitle]}> All you need </Text>
+            <Text style={styles.title}> Here!</Text>
+            </View>
+            
+            <Button title="Sign Up" />
+
+
+            <Pressable hitSlop={20}>
+            <Text style={styles.footerText}>Sign In</Text>
+            </Pressable> 
+        </View>
+    )
+}
+
+export default Splash
