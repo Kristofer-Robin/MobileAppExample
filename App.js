9f325a8bd372ffd2a6aae2a7c13918bc8312bfe2
Student <student@LT-27588.khk.local>
2023-10-04 11:31:16 +0300

    enter

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {SafeAreaView} from 'react-native'
-import Splash from './src/screens/auth/Splash';
 import Signup from './src/screens/auth/Signup';
+
+const WEB_CLIENT_ID = '250697628934-dnjmktrtdn07j7bv5514flj3dr0k8k04.apps.googleusercontent.com'
+const IOS_CLIENT_ID = ''
+const REVERSED_CLIENT_ID = ''
 
 const App = () => {
   return (
